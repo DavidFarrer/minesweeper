9b090389973a6ffdefbd7a18ea53b08566dd78de
Dave Farrer <dave.farrer89@gmail.com>
2018-04-01 14:32:33 -0400

    Revert to 0b7a7e0d07012e69404eff14b0562078d4ba6ca4

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const webpack = require("webpack");
+const path = require("path");
+
+module.exports = {
+  entry: [
+    "react-hot-loader/patch",
+    "./src/index.js"
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      },
+      {
+        test:/\.css$/,
+        use:["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|jp(e*)g|svg)$/,  
+        use: [{
+            loader: 'url-loader',
+            options: { 
+                limit: 8000, // Convert images < 8kb to base64 strings
+                name: 'images/[hash]-[name].[ext]'
+            } 
+        }]
+      }
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname + "/dist"),
+    publicPath: "/",
+    filename: "bundle.js"
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    contentBase: "./dist",
+    hot: true
+  }
+};
